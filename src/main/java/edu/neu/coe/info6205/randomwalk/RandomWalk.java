9e1b399b9d32123ba68b72ecc2ba3349f1d03e49
DJ <dai.jing1@northeastern.edu>
2021-09-15 11:52:34 -0400

    assignment1:add method code

--- src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -20,7 +20,8 @@ public class RandomWalk {
      * @param dy the distance he moves in the y direction
      */
     private void move(int dx, int dy) {
-        // TO BE IMPLEMENTED
+        x += dx;
+        y += dy;
     }
 
     /**
@@ -29,7 +30,9 @@ public class RandomWalk {
      * @param m the number of steps the drunkard takes
      */
     private void randomWalk(int m) {
-        // TO BE IMPLEMENTED
+        for (int i = 0; i < m; i++) {
+            randomMove();
+        }
     }
 
     /**
@@ -48,8 +51,8 @@ public class RandomWalk {
      * @return the (Euclidean) distance from the origin to the current position.
      */
     public double distance() {
-        // TO BE IMPLEMENTED
-        return 0;
+        double d = Math.sqrt(x*x + y*y);
+        return d;
     }
 
     /**
@@ -70,6 +73,17 @@ public class RandomWalk {
     }
 
     public static void main(String[] args) {
+//        if (args.length == 0)
+//            throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
+      for(int i=0;i<10; i++){
+          int m =(int)(Math.random()*200 + 1);
+          int n = 1000;
+//        if (args.length > 1) n = Integer.parseInt(args[1]);
+          double meanDistance = randomWalkMulti(m, n);
+          System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
+      }
+    }
+/*    public static void main(String[] args) {
         if (args.length == 0)
             throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
         int m = Integer.parseInt(args[0]);
@@ -77,6 +91,6 @@ public class RandomWalk {
         if (args.length > 1) n = Integer.parseInt(args[1]);
         double meanDistance = randomWalkMulti(m, n);
         System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
-    }
+    }*/
 
 }
